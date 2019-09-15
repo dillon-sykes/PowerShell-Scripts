b1811879e4c700634de9cb3e148aa08c30807dfc
Dillon Sykes <dillonsykes@gmail.com>
2019-09-15 12:33:03 +1200

    Added scripts

---
+++ AutoTask/CreateAutoTaskTicket.ps1
@@ -0,0 +1,85 @@
+﻿<#
+    .NOTES
+    --------------------------------------------------------------------------------
+     Script: 			 CreateAutoTaskTicket.ps1
+     Created on: 	     24/06/2019 12:00 PM
+     Author:    		 Dillon Sykes
+    --------------------------------------------------------------------------------
+    .DESCRIPTION
+        This script automates creating tickets in AutoTask by using an AutoTask PowerShell module and the AutoTask API.
+		After creating a ticket the script will then add a time entry and complete the ticket.
+		
+	.REQUIREMENTS
+        This script requires the following: 
+		- The AutoTask module, download here: https://www.powershellgallery.com/packages/Autotask
+		- AutoTask credentials
+		- AutoTask API key
+#>
+
+# Import the AutoTask module
+Import-Module AutoTask
+
+$username = 'username@email.com' # Your AutoTask username
+$password = 'password' # Your AutoTask password
+$password = ConvertTo-SecureString $password -AsPlainText -Force
+$ApiKey = "your-api-key"
+$credentials = New-Object System.Management.Automation.PSCredential("\$username",$password)
+ 
+"Connecting to Autotask Web API.."
+Connect-AutotaskWebAPI -Credential $Credentials -ApiTrackingIdentifier $ApiKey
+
+"Connected. Creating ticket.."
+$AccountID = 0 # Company reference number
+$Contact = Get-AtwsContact -EMailAddress 'contactexample@email.com'
+$Description = 'Server maintenance completed, please change me.'
+$DueDateTime = (Get-Date).AddHours(72) # Optional: Set a due date on the ticket. This is set to be due in 3 days.
+$AllocationCodeID = 29682800 # 29682801 = Remote Desktop/Workshop, 29682804 = Maintenence, 29682800 = Onsite Support, 29682861 = Non Billable Support, 29683328 = Travel
+$IssueType = 'Break/Fix' # Service Request
+$SubIssueType = 'Computer' # Options for Computer category: Antivirus, Backup, Computer, E-mail, Internet, Network, Other, Printer, Server, Software, VoIP/Phone, Website
+$Priority = 'Normal' # Emergency, High, Low, Normal, Urgent"
+$TicketType = 'Problem' # Service Request
+$Source = 2 # Call
+$QueueId = 'Support' # Optional: Set the QueueId. "Support, Internal"
+$AssignedResourceID = 29682896
+$CreatorResourceID = 29682896
+$ResourceRole = 29683355 # If your resource has more than 1 role for a particular queue, you will need to specify the Role Id as well
+
+$Status = 'New' # Options: New, In Progress, Scheduled, Complete
+$Title = 'Server Maintenence - Example Client'
+$Resolution = 'This is a test description, please change me.'
+$PurchaseOrderNumber = 'POEX-22082019'
+
+$Ticket_Result = $Ticket = New-AtwsTicket -AccountId $AccountId -ContactID $Contact.ID -Description $Description -DueDateTime $DueDateTime -AllocationCodeID $AllocationCodeID -IssueType $IssueType -SubIssueType $SubIssueType -Priority $Priority -TicketType $TicketType -Source $Source -QueueId $QueueId -AssignedResourceID $AssignedResourceID -CreatorResourceID $CreatorResourceID -AssignedResourceRoleID $ResourceRole -Status $Status -Title $Title -Resolution $Resolution -PurchaseOrderNumber $PurchaseOrderNumber
+# Debugging purposes:
+# Write-Output $Ticket.TicketNumber
+
+if ($Ticket.TicketNumber -eq $null -eq $false) {
+	Write-Host 'SUCCESS: ' -f Green -nonewline; "Ticket created, ticket number: " + $Ticket.TicketNumber
+	$TicketID = $Ticket.id
+	$DateWorked = Get-Date # Required
+	$StartDateTime = Get-Date
+	$EndDateTime = (Get-Date).AddMinutes(15)
+	$HoursWorked = 0.2500
+	$HoursToBill = 0.2500
+	$OffsetHours = 0.0000
+	$SummaryNotes = 'Ticket notes example, please change me.'
+	$ResourceID = $Resource.id
+	$RoleID = $Resource.DefaultServiceDeskRoleID
+	$NonBillable = $true
+
+	"Creating time entry.."
+	$TimeEntry_Result = New-AtwsTimeEntry -TicketID $TicketID -DateWorked $DateWorked -StartDateTime $StartDateTime -EndDateTime $EndDateTime -HoursWorked $HoursWorked -HoursToBill $HoursToBill -OffsetHours $OffsetHours -SummaryNotes $SummaryNotes -ResourceID $AssignedResourceID -RoleID $ResourceRole -NonBillable $NonBillable
+
+	if ($TimeEntry_Result.TicketNumber -eq $null -eq $true) {
+		Write-Host 'SUCCESS: ' -f Green -nonewline; "Time entry created."
+		"Completing ticket.."
+		Set-AtwsTicket -TicketID $Ticket.TicketNumber -Status "Complete"
+		"Script completed successfully."
+	} else {
+		$TimeEntry_Result.Errors
+		"Error - Time entry has NOT been created."
+	}
+} else {
+	$Ticket_Result.Errors
+ "Error - Ticket has NOT been created."
+}
---
+++ Windows/EmailStoppedServices.ps1
@@ -0,0 +1,32 @@
+<#
+    .NOTES
+    --------------------------------------------------------------------------------
+     Script: 			 EmailStoppedServices.ps1
+     Created on: 	     12/05/2019 12:00 PM
+     Author:    		 Dillon Sykes
+    --------------------------------------------------------------------------------
+    .DESCRIPTION
+        This script will start all services set to "Automatic" or "Automatic (Delayed start)" that are in a "Stopped" state.
+		After attempting to start these services, the script will email the services that failed to start.
+#>
+
+$Client="Client Name"
+
+# Email parameters
+		$From = "youremail@email.com"
+		$To = "recipient@gmail.com"
+		$SMTPServer = "smtp.emailserver.com"
+		$Body = "Starting the following services on: ""$($env:computername)""`n"
+		$Subject = " Service Status for $Client $($env:computername) "
+
+# Start all "Automatic" and "Automatic (Delayed start)" services that are in a "Stopped" state
+Get-Service | Select-Object -Property Name,Status,StartType | Where-Object {$_.Status -eq "Stopped" -and $_.StartType -eq "Automatic"} | Start-Service
+
+# Give the services a chance to completely start
+timeout /T 10
+
+# Get all services that are set to "Automatic" and "Automatic (Delayed start)" that are still in a "Stopped" state
+$Body += (Get-Service | Select-Object -Property Name,Status,StartType | Where-Object {$_.Status -eq "Stopped" -and $_.StartType -eq "Automatic"} | Out-String)
+
+# Send email
+Send-MailMessage -from $From -to $To -subject $Subject -body $Body -smtpserver $SMTPServer
