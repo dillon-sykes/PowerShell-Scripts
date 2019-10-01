d2e76cdc29d0d6ac9bd84376d8bae4846dac3b7d
Dillon Sykes <dillonsykes@gmail.com>
2019-10-01 22:23:49 +1300

    Add scripts

---
+++ Active Directory/Get-ADUsersPasswordExpireDate.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+    This script will report the expiry dates and times of all user passwords in AD.
+    
+.DESCRIPTION
+    This script will report the expiry date and time of all Active Directory users with passwords set to expire.
+	Users who have passwords set to never expire will not be reported on.
+    
+.NOTES
+    Dillon Sykes
+    dillonsykes.com
+    github.com/dillon-sykes
+#>
+
+# To find the date the password was last set, run the following command.
+# Get-ADUser -filter * -Properties PasswordLastSet, PasswordNeverExpires |ft Name, PasswordLastSet, PasswordNeverExpires
+
+# To display the expiration date rather than the password last set date, use this command.
+Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" |
+Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}
