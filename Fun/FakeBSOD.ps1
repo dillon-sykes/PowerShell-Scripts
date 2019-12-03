3441822f65c09b8b9e252469aff12e6d933298f4
Dillon Sykes <dillonsykes@gmail.com>
2019-12-04 10:44:48 +1300

    Create FakeBSOD.ps1

---
+++ Fun/FakeBSOD.ps1
@@ -0,0 +1,56 @@
+<#
+.SYNOPSIS
+    Prank users with a fake blue screen of death.
+    
+.DESCRIPTION
+    This script replicates the appearance of a blue screen of death, this is an error screen displayed on a	Windows computer system following a fatal system error.
+#>
+
+$Text = @'
+A problem has been detected and Windows has been shut down to prevent damage 
+to your computer.
+
+CRITICAL_SERVICE_FAILED
+
+If this is the first time you've seen this Stop error screen, 
+restart your computer. If this screen appears again, follow
+these steps:
+
+Check to make sure any new hardware or software is properly installed.
+If this is a new installation, ask your hardware or software manufacturer
+for any Windows updates you might need.
+
+If problems continue, disable or remove any newly installed hardware 
+or software. Disable BIOS memory options such as caching or shadowing.
+If you need to use Safe Mode to remove or disable components, restart
+your computer, press F8 to select Advanced Startup Options, and then
+select Safe Mode.
+
+Technical Information:
+
+*** STOP: 0x0000005A (0x00000001, 0x00000001, 0x00000000, 0x00000000)
+
+Collecting data for crash dump ...
+Initializing disk for crash dump ...
+Beginning dump for physical memory.
+Dumping physical memory to disk: 100
+Physical memory dump complete.
+Contact your system admin or technical support group for further assistance.
+'@
+Add-Type -AssemblyName System.Windows.Forms
+
+$Label = New-Object System.Windows.Forms.Label
+$Label.TabIndex = 1
+$Label.Text = $Text
+$Label.ForeColor = 'White'
+$Label.AutoSize = $True
+$Label.Font = "Lucida Console, 16pt, style=Regular"
+$Label.Location = '0, 30'
+
+$Form = New-Object system.Windows.Forms.Form
+$Form.Controls.Add($Label)
+$Form.WindowState = 'Maximized'
+$Form.FormBorderStyle = 'None'
+$Form.BackColor = "#000080"
+$Form.Cursor=[System.Windows.Forms.Cursors]::WaitCursor
+$Form.ShowDialog()
